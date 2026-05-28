b8be2e81f978bf0689c1aee9733c8066f38b8471
liambui11 <buikinhluan0101@gmail.com>
2026-05-28 17:52:10 +0700

    first commit

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Hospital Management System",
+    "version": "17.0.1.1",
+    "author": "Liam",
+    "license": "LGPL-3",
+    "depends": ["base","mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/patient_views.xml",
+        "views/patient_readonly_views.xml",
+        "views/menu.xml"
+    ],
+    "application": True,
+}
