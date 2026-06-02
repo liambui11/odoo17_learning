7b236cdbf7002c5121093db16dca0d60aba4a704
liambui11 <buikinhluan0101@gmail.com>
2026-06-02 23:31:26 +0700

    feat: add sequence

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -3,11 +3,13 @@
     "version": "17.0.1.1",
     "author": "Liam",
     "license": "LGPL-3",
-    "depends": ["base","mail"],
+    "depends": ["base", "mail"],
     "data": [
         "security/ir.model.access.csv",
+        "data/sequence.xml",
         "views/patient_views.xml",
         "views/patient_readonly_views.xml",
+        "views/appointment_views.xml",
         "views/menu.xml"
     ],
     "application": True,
---
+++ custom_addons/om_hospital/models/appointment.py
@@ -0,0 +1,21 @@
+from odoo import api, fields, models
+
+
+class HospitalAppointment(models.Model):
+    _name = 'hospital.appointment'
+    _inherit=['mail.thread']
+    _description = 'Hospital Appointment'
+    _rec_name = 'patient_id'
+
+    reference = fields.Char(string='Reference', default='New')
+    patient_id = fields.Many2one('hospital.patient', string='Patient')
+    date_appointment = fields.Date()
+    note = fields.Text(string='Note')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if not vals.get('reference') or vals['reference'] == 'New':
+                vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
+        return super().create(vals_list)
+
